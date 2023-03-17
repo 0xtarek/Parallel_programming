55521060a3b8b49c60ef45befa8c0098e101af75
Mohamed Tarek <7amo7eko2001@gmail.com>
2023-03-17 22:16:16 +0200

    Update second.c

--- second.c
+++ second.c
@@ -2,84 +2,95 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define SIZE 4
-
-int main(int argc, char *argv[]) {
-    int rank, size, i, j, k;
-    int **A, **B, **C;
-    int *buf_A, *buf_B, *buf_C;
-    MPI_Status status;
-
+int main(int argc, char** argv) {
+    int rank, size;
+    int nrows, ncols;
+    int* a = NULL;
+    int* b = NULL;
+    int* c = NULL;
+    int* row = NULL;
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    // Allocate memory dynamically for matrices
-    A = (int **) malloc(sizeof(int *) * SIZE);
-    B = (int **) malloc(sizeof(int *) * SIZE);
-    C = (int **) malloc(sizeof(int *) * SIZE);
-    for (i = 0; i < SIZE; i++) {
-        A[i] = (int *) malloc(sizeof(int) * SIZE);
-        B[i] = (int *) malloc(sizeof(int) * SIZE);
-        C[i] = (int *) malloc(sizeof(int) * SIZE);
-    }
-
-    // Initialize matrices A and B
-    for (i = 0; i < SIZE; i++) {
-        for (j = 0; j < SIZE; j++) {
-            A[i][j] = i + j;
-            B[i][j] = i * j;
+    
+    if (rank == 0) {
+        printf("Enter the number of rows and columns for matrix A:\n");
+        scanf("%d %d", &nrows, &ncols);
+        
+        a = (int*) malloc(nrows * ncols * sizeof(int));
+        
+        printf("Enter the values of matrix A:\n");
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                scanf("%d", &a[i*ncols+j]);
+            }
         }
-    }
-
-    // Allocate buffers for sending and receiving matrices
-    buf_A = (int *) malloc(sizeof(int) * SIZE * SIZE);
-    buf_B = (int *) malloc(sizeof(int) * SIZE * SIZE);
-    buf_C = (int *) malloc(sizeof(int) * SIZE * SIZE);
-
-    // Scatter matrix A to all processes
-    MPI_Scatter(A[0], SIZE*SIZE/size, MPI_INT, buf_A, SIZE*SIZE/size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Broadcast matrix B to all processes
-    MPI_Bcast(B[0], SIZE*SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Perform matrix multiplication
-    for (i = 0; i < SIZE/size; i++) {
-        for (j = 0; j < SIZE; j++) {
-            C[i][j] = 0;
-            for (k = 0; k < SIZE; k++) {
-                C[i][j] += buf_A[i*SIZE+k] * B[k][j];
+        
+        printf("Enter the number of rows and columns for matrix B:\n");
+        scanf("%d %d", &nrows, &ncols);
+        
+        b = (int*) malloc(nrows * ncols * sizeof(int));
+        
+        printf("Enter the values of matrix B:\n");
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                scanf("%d", &b[i*ncols+j]);
             }
         }
     }
-
-    // Gather all submatrices C back to root process
-    MPI_Gather(C[0], SIZE*SIZE/size, MPI_INT, buf_C, SIZE*SIZE/size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Print the result
+    
+    MPI_Bcast(&nrows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&ncols, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    int chunk_size = nrows / size;
+    int chunk_start = rank * chunk_size;
+    int chunk_end = chunk_start + chunk_size;
+    
+    if (rank == size - 1) {
+        chunk_end = nrows;
+    }
+    
+    row = (int*) malloc(ncols * sizeof(int));
+    
+    int* recv_buffer = (int*) malloc(ncols * sizeof(int));
+    
+    c = (int*) malloc(nrows * ncols * sizeof(int));
+    
+    for (int i = chunk_start; i < chunk_end; i++) {
+        for (int j = 0; j < ncols; j++) {
+            row[j] = 0;
+            for (int k = 0; k < nrows; k++) {
+                row[j] += a[i*ncols+k] * b[k*ncols+j];
+            }
+        }
+        MPI_Reduce(row, recv_buffer, ncols, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (rank == 0) {
+            for (int j = 0; j < ncols; j++) {
+                c[i*ncols+j] = recv_buffer[j];
+            }
+        }
+    }
+    
+    MPI_Gather(c + chunk_start*ncols, chunk_size*ncols, MPI_INT,
+               c, chunk_size*ncols, MPI_INT, 0, MPI_COMM_WORLD);
+    
     if (rank == 0) {
-        printf("Result:\n");
-        for (i = 0; i < SIZE; i++) {
-            for (j = 0; j < SIZE; j++) {
-                printf("%d ", buf_C[i*SIZE+j]);
+        printf("The result of matrix multiplication is:\n");
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                printf("%d ", c[i*ncols+j]);
             }
             printf("\n");
         }
     }
-
-    // Free allocated memory
-    for (i = 0; i < SIZE; i++) {
-        free(A[i]);
-        free(B[i]);
-        free(C[i]);
-    }
-    free(A);
-    free(B);
-    free(C);
-    free(buf_A);
-    free(buf_B);
-    free(buf_C);
+    
+    free(a);
+    free(b);
+    free(c);
+    free(row);
 
     MPI_Finalize();
     return 0;
+
 }
