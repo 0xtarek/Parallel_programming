65be6c717f67ad49ea9c722ee55343970f3fc2de
Mohamed Tarek <7amo7eko2001@gmail.com>
2023-03-17 21:36:42 +0200

    Update second.c

--- second.c
+++ second.c
@@ -2,85 +2,84 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define ROWS 3
-#define COLS 3
+#define SIZE 4
 
 int main(int argc, char *argv[]) {
     int rank, size, i, j, k;
-    int **matrix_a, **matrix_b, **matrix_c;
-    int *send_buffer, *recv_buffer;
+    int **A, **B, **C;
+    int *buf_A, *buf_B, *buf_C;
     MPI_Status status;
 
-    // Initialize MPI environment
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Allocate memory for matrices
-    matrix_a = (int **) malloc(ROWS * sizeof(int *));
-    matrix_b = (int **) malloc(COLS * sizeof(int *));
-    matrix_c = (int **) malloc(ROWS * sizeof(int *));
-    for (i = 0; i < ROWS; i++) {
-        matrix_a[i] = (int *) malloc(COLS * sizeof(int));
-        matrix_c[i] = (int *) malloc(COLS * sizeof(int));
-    }
-    for (i = 0; i < COLS; i++) {
-        matrix_b[i] = (int *) malloc(ROWS * sizeof(int));
+    // Allocate memory dynamically for matrices
+    A = (int **) malloc(sizeof(int *) * SIZE);
+    B = (int **) malloc(sizeof(int *) * SIZE);
+    C = (int **) malloc(sizeof(int *) * SIZE);
+    for (i = 0; i < SIZE; i++) {
+        A[i] = (int *) malloc(sizeof(int) * SIZE);
+        B[i] = (int *) malloc(sizeof(int) * SIZE);
+        C[i] = (int *) malloc(sizeof(int) * SIZE);
     }
 
-    // Initialize matrices
+    // Initialize matrices A and B
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            A[i][j] = i + j;
+            B[i][j] = i * j;
+        }
+    }
+
+    // Allocate buffers for sending and receiving matrices
+    buf_A = (int *) malloc(sizeof(int) * SIZE * SIZE);
+    buf_B = (int *) malloc(sizeof(int) * SIZE * SIZE);
+    buf_C = (int *) malloc(sizeof(int) * SIZE * SIZE);
+
+    // Scatter matrix A to all processes
+    MPI_Scatter(A[0], SIZE*SIZE/size, MPI_INT, buf_A, SIZE*SIZE/size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Broadcast matrix B to all processes
+    MPI_Bcast(B[0], SIZE*SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Perform matrix multiplication
+    for (i = 0; i < SIZE/size; i++) {
+        for (j = 0; j < SIZE; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < SIZE; k++) {
+                C[i][j] += buf_A[i*SIZE+k] * B[k][j];
+            }
+        }
+    }
+
+    // Gather all submatrices C back to root process
+    MPI_Gather(C[0], SIZE*SIZE/size, MPI_INT, buf_C, SIZE*SIZE/size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Print the result
     if (rank == 0) {
-        for (i = 0; i < ROWS; i++) {
-            for (j = 0; j < COLS; j++) {
-                matrix_a[i][j] = i + j;
-            }
-        }
-        for (i = 0; i < COLS; i++) {
-            for (j = 0; j < ROWS; j++) {
-                matrix_b[i][j] = i * j;
-            }
-        }
-    }
-
-    // Scatter matrix b to all processes
-    send_buffer = (int *) malloc(ROWS * sizeof(int));
-    recv_buffer = (int *) malloc(ROWS * sizeof(int));
-    MPI_Scatter(matrix_b, ROWS, MPI_INT, send_buffer, ROWS, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Compute local portion of matrix c
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
-            matrix_c[i][j] = 0;
-            for (k = 0; k < ROWS; k++) {
-                matrix_c[i][j] += matrix_a[i][k] * send_buffer[k];
-            }
-        }
-    }
-
-    // Gather local portions of matrix c to process 0
-    MPI_Gatherv(matrix_c, ROWS*COLS/size, MPI_INT, matrix_c, ROWS*COLS/size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Print result
-    if (rank == 0) {
-        printf("Matrix A:\n");
-        for (i = 0; i < ROWS; i++) {
-            for (j = 0; j < COLS; j++) {
-                printf("%d ", matrix_a[i][j]);
+        printf("Result:\n");
+        for (i = 0; i < SIZE; i++) {
+            for (j = 0; j < SIZE; j++) {
+                printf("%d ", buf_C[i*SIZE+j]);
             }
             printf("\n");
         }
-        printf("Matrix B:\n");
-        for (i = 0; i < COLS; i++) {
-            for (j = 0; j < ROWS; j++) {
-                printf("%d ", matrix_b[i][j]);
-            }
-            printf("\n");
-        }
-        printf("Matrix C:\n");
-        for (i = 0; i < ROWS; i++) {
-            for (j = 0; j < COLS; j++){
-                printf("%d ", matrix_c[i][j]);
-            }
-        }
     }
+
+    // Free allocated memory
+    for (i = 0; i < SIZE; i++) {
+        free(A[i]);
+        free(B[i]);
+        free(C[i]);
+    }
+    free(A);
+    free(B);
+    free(C);
+    free(buf_A);
+    free(buf_B);
+    free(buf_C);
+
+    MPI_Finalize();
+    return 0;
 }
