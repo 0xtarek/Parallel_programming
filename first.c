b4f6ade8798e0341591ffe279a6f7f451862aa63
Mohamed Tarek <7amo7eko2001@gmail.com>
2023-03-17 21:54:39 +0200

    Update first.c

--- first.c
+++ first.c
@@ -1,64 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <mpi.h>
 
-#define N 100 // size of array
+#define ARRAY_SIZE 100
 
-int main(int argc, char *argv[]) {
-    int rank, size, i, tag = 0;
-    int *arr, *subarr;
-    int numElements, maxElement = 0;
-
+int main(int argc, char** argv) {
+    int rank, size;
+    int array[ARRAY_SIZE];
+    int max = 0;
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    numElements = N/size;
-    subarr = (int *) malloc(numElements*sizeof(int));
-
-    if(rank == 0) {
-        // initialize array
-        arr = (int *) malloc(N*sizeof(int));
-        for(i = 0; i < N; i++) {
-            arr[i] = i;
+    
+    if (rank == 0) {
+        printf("Enter %d numbers:\n", ARRAY_SIZE);
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            scanf("%d", &array[i]);
         }
-
-        // scatter array to all processes
-        for(i = 1; i < size; i++) {
-            MPI_Send(arr+(i*numElements), numElements, MPI_INT, i, tag, MPI_COMM_WORLD);
+        
+        for (int dest = 1; dest < size; dest++) {
+            MPI_Send(array, ARRAY_SIZE, MPI_INT, dest, 0, MPI_COMM_WORLD);
         }
-        subarr = arr;
+    } else {
+        MPI_Recv(array, ARRAY_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-    else {
-        // receive subarray from process 0
-        MPI_Recv(subarr, numElements, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    
+    int chunk_size = ARRAY_SIZE / size;
+    int chunk_start = rank * chunk_size;
+    int chunk_end = chunk_start + chunk_size;
+    
+    if (rank == size - 1) {
+        chunk_end = ARRAY_SIZE;
     }
-
-    // find max element in subarray
-    for(i = 0; i < numElements; i++) {
-        if(subarr[i] > maxElement) {
-            maxElement = subarr[i];
+    
+    for (int i = chunk_start; i < chunk_end; i++) {
+        if (array[i] > max) {
+            max = array[i];
         }
     }
-
-    // send max element to process 0
-    if(rank != 0) {
-        MPI_Send(&maxElement, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
-    }
-    else {
-        // receive max element from all other processes
-        int temp;
-        for(i = 1; i < size; i++) {
-            MPI_Recv(&temp, 1, MPI_INT, i, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            if(temp > maxElement) {
-                maxElement = temp;
+    
+    if (rank != 0) {
+        MPI_Send(&max, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    } else {
+        for (int source = 1; source < size; source++) {
+            int local_max;
+            MPI_Recv(&local_max, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (local_max > max) {
+                max = local_max;
             }
         }
-
-        // print the results
-        printf("The maximum element in the array is %d\n", maxElement);
+        printf("The maximum number is %d\n", max);
     }
-
+    
     MPI_Finalize();
+    
     return 0;
 }
