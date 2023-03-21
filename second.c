6006bcf7884f1397bad68dbca8adeeec532bf4e4
Mohamed Tarek <7amo7eko2001@gmail.com>
2023-03-21 15:05:51 +0200

    Update second.c

--- second.c
+++ second.c
@@ -2,95 +2,71 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+#define ROWS_A 3
+#define COLS_A 3
+#define ROWS_B 3
+#define COLS_B 3
+
 int main(int argc, char** argv) {
     int rank, size;
-    int nrows, ncols;
-    int* a = NULL;
-    int* b = NULL;
-    int* c = NULL;
-    int* row = NULL;
-    
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
+    if (size != 2) {
+        printf("Error: This program should be run with 2 processes\n");
+        MPI_Finalize();
+        return 1;
+    }
+
     if (rank == 0) {
-        printf("Enter the number of rows and columns for matrix A:\n");
-        scanf("%d %d", &nrows, &ncols);
-        
-        a = (int*) malloc(nrows * ncols * sizeof(int));
-        
-        printf("Enter the values of matrix A:\n");
-        for (int i = 0; i < nrows; i++) {
-            for (int j = 0; j < ncols; j++) {
-                scanf("%d", &a[i*ncols+j]);
-            }
-        }
-        
-        printf("Enter the number of rows and columns for matrix B:\n");
-        scanf("%d %d", &nrows, &ncols);
-        
-        b = (int*) malloc(nrows * ncols * sizeof(int));
-        
-        printf("Enter the values of matrix B:\n");
-        for (int i = 0; i < nrows; i++) {
-            for (int j = 0; j < ncols; j++) {
-                scanf("%d", &b[i*ncols+j]);
-            }
-        }
-    }
-    
-    MPI_Bcast(&nrows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&ncols, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    int chunk_size = nrows / size;
-    int chunk_start = rank * chunk_size;
-    int chunk_end = chunk_start + chunk_size;
-    
-    if (rank == size - 1) {
-        chunk_end = nrows;
-    }
-    
-    row = (int*) malloc(ncols * sizeof(int));
-    
-    int* recv_buffer = (int*) malloc(ncols * sizeof(int));
-    
-    c = (int*) malloc(nrows * ncols * sizeof(int));
-    
-    for (int i = chunk_start; i < chunk_end; i++) {
-        for (int j = 0; j < ncols; j++) {
-            row[j] = 0;
-            for (int k = 0; k < nrows; k++) {
-                row[j] += a[i*ncols+k] * b[k*ncols+j];
-            }
-        }
-        MPI_Reduce(row, recv_buffer, ncols, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-        if (rank == 0) {
-            for (int j = 0; j < ncols; j++) {
-                c[i*ncols+j] = recv_buffer[j];
-            }
-        }
-    }
-    
-    MPI_Gather(c + chunk_start*ncols, chunk_size*ncols, MPI_INT,
-               c, chunk_size*ncols, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    if (rank == 0) {
-        printf("The result of matrix multiplication is:\n");
-        for (int i = 0; i < nrows; i++) {
-            for (int j = 0; j < ncols; j++) {
-                printf("%d ", c[i*ncols+j]);
+        // Initialize matrices A and B
+        int A[ROWS_A][COLS_A] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int B[ROWS_B][COLS_B] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+
+        // Send matrix A to process 1
+        MPI_Send(A, ROWS_A * COLS_A, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+        // Send matrix B to process 1
+        MPI_Send(B, ROWS_B * COLS_B, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+        // Receive the product from process 1
+        int C[ROWS_A][COLS_B];
+        MPI_Recv(C, ROWS_A * COLS_B, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Print the product matrix
+        printf("Product:\n");
+        for (int i = 0; i < ROWS_A; i++) {
+            for (int j = 0; j < COLS_B; j++) {
+                printf("%d ", C[i][j]);
             }
             printf("\n");
         }
     }
-    
-    free(a);
-    free(b);
-    free(c);
-    free(row);
+    else if (rank == 1) {
+        // Receive matrix A from process 0
+        int A[ROWS_A][COLS_A];
+        MPI_Recv(A, ROWS_A * COLS_A, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Receive matrix B from process 0
+        int B[ROWS_B][COLS_B];
+        MPI_Recv(B, ROWS_B * COLS_B, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Compute the product matrix
+        int C[ROWS_A][COLS_B];
+        for (int i = 0; i < ROWS_A; i++) {
+            for (int j = 0; j < COLS_B; j++) {
+                C[i][j] = 0;
+                for (int k = 0; k < COLS_A; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        // Send the product matrix to process 0
+        MPI_Send(C, ROWS_A * COLS_B, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
 
     MPI_Finalize();
     return 0;
-
 }
